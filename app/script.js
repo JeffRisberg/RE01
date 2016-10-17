47ab630ee63957aaf1dd24401376b36ecb24846c
JeffRisberg <jrisberg@aol.com>
2016-10-16 19:56:02 -0700

    moved babel into the server side

--- app/script.js
+++ app/script.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 class UserList extends React.Component {
     constructor() {
         super();
--- server.js
+++ server.js
@@ -5,10 +5,19 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 3000));
+const PATH_STYLES = path.resolve(__dirname, 'app/styles');
+const PATH_DIST = path.resolve(__dirname, 'dist');
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/styles', express.static(PATH_STYLES));
+app.use(express.static(PATH_DIST));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'app/index.html'));
+});
+
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function () {
     console.log('Server is listening at %s', app.get('port'));
 });
+
